c9ca147ac943f294aba0f299bff89b1ac4fb78f0
Fahra Akhlaq <f.akhlaq@outlook.com>
2022-02-10 22:15:58 +0000

    created about me section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Intro from "./components/intro/Intro";
+import About from "./components/about/About";
 import "./App.css";
+
 function App() {
   return (
-    <h1>
+    <div>
       <Intro />
-    </h1>
+      <About />
+    </div>
   );
 }
 
---
+++ src/components/about/About.js
@@ -0,0 +1,17 @@
+import "./about.css";
+
+function About() {
+  return (
+    <div className="about">
+      <div className="about-left">
+        <div className="about-card-bg"></div>
+        <div className="about-card">
+          <img src="" alt="" className=""></img>
+        </div>
+      </div>
+      <div className="about-right"></div>
+    </div>
+  );
+}
+
+export default About;
