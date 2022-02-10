5de8f15438161f84fec91661eb80afed1bd4ef60
Fahra Akhlaq <f.akhlaq@outlook.com>
2022-02-10 12:48:43 +0000

    intro added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import Intro from "./components/intro/Intro";
+import "./App.css";
 function App() {
-  return <h1>hello everyone</h1>;
+  return (
+    <h1>
+      <Intro />
+    </h1>
+  );
 }
 
 export default App;
---
+++ src/components/intro/Intro.js
@@ -0,0 +1,32 @@
+import "./intro.css";
+import me from "../../img/me.png";
+
+function Intro() {
+  return (
+    <div className="intro">
+      <div className="intro-left">
+        <div className="intro-left-wrapper">
+          <h3 className="intro-text">Hello! I'm</h3>
+          <h1 className="intro-name">Fahra Akhlaq</h1>
+          <div className="job-title-container">
+            <div className="job-title-wrapper">
+              <div className="job-title-item">Full Stack Developer</div>
+              <div className="job-title-item">Web Developer</div>
+              <div className="job-title-item">UI/UX Designer</div>
+            </div>
+          </div>
+          <div className="intro-desc">
+            Strengths in creativity, teamwork, and building projects in and
+            agile manner from ideation to execution.
+          </div>
+        </div>
+      </div>
+      <div className="intro-right">
+        <div className="intro-img-bg"></div>
+        <img src={me} alt="" className="intro-img"></img>
+      </div>
+    </div>
+  );
+}
+
+export default Intro;
